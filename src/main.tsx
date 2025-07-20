d576f517cbe79e01bdb884b1b6e6e1bdf95621a6
Gogulan <gogulan.kanagaraj@zf.com>
2025-07-20 15:31:32 +0530

    Fixed some css issues

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,11 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router';
-import { initializeApp } from 'firebase/app';
-import { firebaseConfig } from './firebase/FirebaseConfig.ts';
+
 import 'react-toastify/dist/ReactToastify.css';
-import { getFirestore } from 'firebase/firestore';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-export const appFirebase = initializeApp(firebaseConfig);
-export const db = getFirestore(appFirebase);
+import { initializeFirebase } from './firebase/FirebaseConfig.ts';
+export const { appFirebase, db } = initializeFirebase();
 export const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
